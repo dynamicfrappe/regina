f7db52ad143054cfd0d0e0e0cc9dc833b627bbf9
Mohamed-Baio1 <mo.yassin344@gmail.com>
2024-04-16 15:35:59 +0200

    create unit week doctype

--- regina/unit_reservation/doctype/unit_week/unit_week.py
+++ regina/unit_reservation/doctype/unit_week/unit_week.py
@@ -3,64 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
-
-# class Unitweek(Document):
-# 	def validate(self):
-# 		self.append("unit_week_tool", {})
-# 		days1 = get_dates_for_day(self)
-
-# 		for day in days1:
-# 			self.append("unit_week_tool" , {
-# 				'start_day':day.start_day,
-# 				'end_day':day.end_day
-# 			})
-	# pass
-	# def on_update(self):
-	#     self.update_child_table_dates()
-
-	# def update_child_table_dates(self):
-	#     from_date = self.from_date
-	#     to_date = self.to_date
-
-	#     if from_date and to_date:
-	#         for row in self.get('unit_week_tool'):
-	#             row.start_day = from_date
-	#             row.end_day = to_date
-
-	#         self.save()  
-	#     else:
-	#         frappe.throw("From date and to date are required.")
-
-
-
-
-# from frappe.utils import cint, formatdate, getdate, today
-
-# @frappe.whitelist()
-# def get_weekly_off_date_list(self , day):
-# 	start_date, end_date = getdate(self.from_date), getdate(self.to_date)
-
-# 	import calendar
-# 	from datetime import timedelta
-
-# 	from dateutil import relativedelta
-
-# 	date_list = []
-# 	existing_date_list = []
-# 	week_1st_day = getattr(calendar, (day).upper())
-# 	# week_2st_day = getattr(calendar, (self.end_day).upper())
-# 	reference_date = start_date + relativedelta.relativedelta(weekday=week_1st_day)
-# 	# existing_date_list = [getdate(holiday.holiday_date) for holiday in self.get("holidays")]
-
-# 	while reference_date <= end_date:
-# 		# if reference_date not in existing_date_list:
-# 		date_list.append(reference_date)
-# 		reference_date += timedelta(days=7)
-# 	return date_list
-
-
 from datetime import datetime, timedelta
-import frappe
 
 class Unitweek(Document):
     def validate(self):
@@ -106,48 +49,6 @@ def get_dates_for_day(from_date, to_date, start_day, end_day):
 
 
 
-
-
-
-
-
-
-		
-
-from datetime import datetime, timedelta
-
-
-@frappe.whitelist()
-def baio(self):
-    # Convert start_date and end_date strings to datetime objects
-    start = datetime.strptime(str(self.from_date), '%Y-%m-%d')
-    end = datetime.strptime(str(self.to_date), '%Y-%m-%d')
-
-    target_day = self.start_day
-    day = self.end_day
-
-    days_map1 = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
-
-    target_day_int = days_map1[target_day]
-    target_sec_day = days_map1[day]
-
-    matching_dates = {}
-
-    current_date = start
-    # while current_date <= end:
-    #     if current_date.weekday() == target_day_int:
-    #         key = current_date.strftime('%A')  
-    #         if key not in matching_dates:
-    #             matching_dates[key] = []
-    #         matching_dates[key].append(current_date.strftime('%Y-%m-%d'))
-    #     elif current_date.weekday() == target_sec_day:
-    #         key = current_date.strftime('%A')  
-    #         if key not in matching_dates:
-    #             matching_dates[key] = []
-    #         matching_dates[key].append(current_date.strftime('%Y-%m-%d'))
-
-        # current_date += timedelta(days=1)
-    return matching_dates
 
 
 
