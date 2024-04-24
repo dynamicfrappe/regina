5a96ad5243584a775ad4134c6432ac5d4ea95758
Mohamed-Baio1 <mo.yassin344@gmail.com>
2024-04-24 15:44:14 +0200

    modification in contract

--- regina/controllers/contract.py
+++ regina/controllers/contract.py
@@ -6,14 +6,15 @@ from frappe import _
 
 @frappe.whitelist()
 def receive_contract(doc, *args , **kwargs) :
-   name ,agent , contract_type = frappe.db.get_value("Contract Serial Number" , doc ,
-                     ['name' ,'agent' , 'contract_type'])
+   name ,agent , contract_type ,number = frappe.db.get_value("Contract Serial Number" , doc ,
+                     ['name' ,'agent' , 'contract_type' ,'number'])
    document = frappe.get_doc("Contract Serial Number" ,doc)
    terms = frappe.db.get_value("Contract Type" ,contract_type , "contract_terms")
    contract = frappe.new_doc("Contract") 
    contract.party_type = "Agent"
    contract.party_name = agent
-   contract.contract_serial_number = name
+   contract.contract_serial_number = number
+   contract.serial_number = name
    contract.agent = agent
    contract.contract_type = contract_type
    contract.contract_terms = terms
