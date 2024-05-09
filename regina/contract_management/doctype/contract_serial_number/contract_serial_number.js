eb953e97d30c4ebb62b320ff7c6f1afdf3effe02
Baio <mo.yassin344@gmail.com>
2024-05-09 16:54:22 +0300

    refactor

--- regina/contract_management/doctype/contract_serial_number/contract_serial_number.js
+++ regina/contract_management/doctype/contract_serial_number/contract_serial_number.js
@@ -12,19 +12,40 @@ frappe.ui.form.on('Contract Serial Number', {
 	rejected:function(frm){
 		frm.events.setup_buttons(frm)
 	},
-	setup_buttons:function(frm){
+	// setup_buttons:function(frm){
+	// 	if (!frm.doc.contract && frm.doc.status == "On Progress" && !frm.doc.rejected) {
+	// 		frm.add_custom_button(__('Create Contract'), function(){
+	// 			frm.events.create_contract(frm)
+	// 	  }, __("Contracts"));
+	// 	} else {
+
+	// 	frm.remove_custom_button("Create Contract" ,"Contracts")
+	// 			}
+		
+		
+
+	// },
+	setup_buttons: function(frm) {
 		if (!frm.doc.contract && frm.doc.status == "On Progress" && !frm.doc.rejected) {
-			frm.add_custom_button(__('Create Contract'), function(){
-				frm.events.create_contract(frm)
-		  }, __("Contracts"));
+			frm.add_custom_button(__('Create Contract'), function() {
+				frappe.call({
+					method: 'regina.contract_management.doctype.contract_serial_number.contract_serial_number.process_contract_serial_number',
+					args: {
+						contract_serial_number: frm.doc.contract_serial_number
+					},
+					callback: function(r) {
+						if(r.message) {
+							frappe.msgprint(r.message);
+						} else {
+						}
+					}
+				});
+			}, __("Contracts"));
 		} else {
-
-		frm.remove_custom_button("Create Contract" ,"Contracts")
-				}
-		
-		
-
+			frm.remove_custom_button("Create Contract", "Contracts");
+		}
 	},
+	
 	create_contract:function(frm){
 		frappe.call({
 			"method" : "regina.controllers.contract.receive_contract" , 
--- regina/contract_management/doctype/contract_serial_number/contract_serial_number.py
+++ regina/contract_management/doctype/contract_serial_number/contract_serial_number.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 from frappe.utils import today  
 
@@ -26,4 +27,27 @@ def send_to_agent(self ,agent) :
 
 def receive_contract(self , date) :
 	pass
+
+
+
+@frappe.whitelist()
+def check_agent_and_status(contract_serial_number):
+    contract_agent = frappe.db.get_value("Contract", contract_serial_number, "agent")
+    payment_paper_receive = frappe.get_all("Payment Paper Receive", filters={"agent": contract_agent}, fields=["name"])
+    
+    for ppr in payment_paper_receive:
+        valid_items = frappe.get_all("Payment Paper Receive Item", filters={"parent": ppr.name, "status": "Valid"}, fields=["name"])
+        total_items = frappe.get_all("Payment Paper Receive Item", filters={"parent": ppr.name}, fields=["name"])
+        
+        if len(valid_items) != len(total_items):
+            return False
+    
+    return True 
+
+          
+     
+
+
+
+
 
